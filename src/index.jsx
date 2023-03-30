adc67b6b6bce4c79b16e834239a8acf830831b88
Nuria Suarez <nuria.suarez@sitecore.com>
2023-03-30 13:13:22 -0300

    Remove server worker registration

--- src/index.jsx
+++ src/index.jsx
@@ -3,13 +3,7 @@ import ReactDOM from 'react-dom/client';
 // import "./index.css";
 import App from './App';
 import './logLevel';
-import * as serviceWorker from './serviceWorker';
 import './sitecoreWidgetsConfig';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
